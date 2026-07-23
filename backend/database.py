eb14b13313d9129b86e8cffd044004e4a5813785
labdhi-jain <jainlabdhi1405@gmail.com>
2026-07-23 23:17:25 +0530

    Remove unused user-related functions from database.py

--- backend/database.py
+++ backend/database.py
@@ -76,30 +76,3 @@ def get_emotions():
 
     conn.close()
     return data
-
-def create_user(username, password):
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "INSERT INTO users (username, password) VALUES (?, ?)",
-        (username, password)
-    )
-
-    conn.commit()
-    conn.close()
-
-
-def get_user(username):
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "SELECT * FROM users WHERE username=?",
-        (username,)
-    )
-
-    user = cursor.fetchone()
-    conn.close()
-
-    return user
